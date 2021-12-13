4d3515d85723ba1aec819543bcdbd4bacc16b501
milyukov <milyukov@protei-lab.ru>
2021-12-13 15:13:17 +0300

    EDU-228 beginning

--- src/component/navigation/Navigation.tsx
+++ src/component/navigation/Navigation.tsx
@@ -42,14 +42,15 @@ export const Navigation = observer(function Navigation() {
                       data-bs-dismiss="offcanvas"
                       onClick={onLogOut}
                       aria-label="Close">
-                <svg xmlns="http://www.w3.org/2000/svg"
-                     width="32" height="32"
-                     fill="currentColor"
-                     className="bi bi-caret-left"
-                     viewBox="0 0 16 16">
-                  <path
-                    d="M10 12.796V3.204L4.519 8 10 12.796zm-.659.753-5.48-4.796a1 1 0 0 1 0-1.506l5.48-4.796A1 1 0 0 1 11 3.204v9.592a1 1 0 0 1-1.659.753z"/>
+                <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-door-open" fill="currentColor"
+                     xmlns="http://www.w3.org/2000/svg">
+                  <path fillRule="evenodd"
+                        d="M1 15.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5zM11.5 2H11V1h.5A1.5 1.5 0 0 1 13 2.5V15h-1V2.5a.5.5 0 0 0-.5-.5z"/>
+                  <path fillRule="evenodd"
+                        d="M10.828.122A.5.5 0 0 1 11 .5V15h-1V1.077l-6 .857V15H3V1.5a.5.5 0 0 1 .43-.495l7-1a.5.5 0 0 1 .398.117z"/>
+                  <path d="M8 9c0 .552.224 1 .5 1s.5-.448.5-1-.224-1-.5-1-.5.448-.5 1z"/>
                 </svg>
+
               </button>
             </div>
             <div className="offcanvas-body">
