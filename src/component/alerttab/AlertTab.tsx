de14a031ef0c323132bdc6e2ba6c5e8903bc5f48
milyukov <milyukov@protei-lab.ru>
2022-01-10 11:03:38 +0300

    240 fix display time in journal table fix alert

--- src/component/alerttab/AlertTab.tsx
+++ src/component/alerttab/AlertTab.tsx
@@ -3,10 +3,16 @@ import React from "react";
 import {webSocketStore} from "../../store/webSocketStore";
 import {DogController} from "../../controller/DogController";
 import {alertShowStore} from "../../store/AlertShowStore";
+import {useTranslation} from "react-i18next";
 
 
 export const AlertTab = observer(function AlertTab() {
+
+  const {t} = useTranslation();
   const isShown = alertShowStore.isAlertShown
+  const socket: boolean = webSocketStore.isWebSocketOpen
+  const alertClass: string = (socket) ? "alert alert-secondary alert-tab" : "alert alert-danger alert-tab"
+  const alertText = (socket) ? "AlertTab.connect" : "AlertTab.disconnect"
 
   DogController.alertShow()
 
@@ -14,17 +20,10 @@ export const AlertTab = observer(function AlertTab() {
     return <div/>
   }
 
-  if (!webSocketStore.isWebSocketOpen) {
-    return <div>
-      <div className="alert alert-danger alert-tab" role="alert">
-        Вы не подключились к серверу!
-      </div>
+  return <div>
+    <div className={alertClass} role="alert">
+      {t(alertText)}
     </div>
-  } else {
-    return <div>
-      <div className="alert alert-secondary alert-tab" role="alert">
-        Вы подключились к серверу! Сервер приветствует Собак!
-      </div>
-    </div>
-  }
+  </div>
+
 })
